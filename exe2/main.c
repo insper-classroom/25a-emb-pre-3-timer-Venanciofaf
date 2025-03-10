a82666607a3b8fd8f4702febb3919f0ef0f7b952
Venanciofaf <venanciofaf@al.insper.edu.br>
2025-03-10 10:19:48 -0300

    Códigos 1 e 2 feitos

--- exe2/main.c
+++ exe2/main.c
@@ -20,6 +20,21 @@ void btn_callback(uint gpio, uint32_t events) {
     }
 }
 
+bool timer_red_callback(repeating_timer_t *rt) {
+    bool current_state = gpio_get(LED_PIN_R);
+    gpio_put(LED_PIN_R , !current_state);
+    return true; 
+}
+
+
+bool timer_green_callback(repeating_timer_t *rt) {
+    bool current_state = gpio_get(LED_PIN_G);
+    gpio_put(LED_PIN_G , !current_state);
+    return true; 
+}
+
+
+
 int main() {
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
@@ -39,14 +54,42 @@ int main() {
                                        &btn_callback);
     gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
+    repeating_timer_t timer_red;
+    bool time_red_active = false;
+    
+    repeating_timer_t timer_green;
+    bool time_green_active = false;
+
     while (true) {
 
         if (flag_r) {
             flag_r = 0;
+            
+            if (time_red_active){
+                cancel_repeating_timer(&timer_red);
+                gpio_put(LED_PIN_R , 0);
+                time_red_active = false;
+
+            }
+            else{
+                add_repeating_timer_ms(500 , timer_red_callback , NULL , &timer_red);
+                time_red_active = true;
+            }
         }
 
         if (flag_g) {
             flag_g = 0;
+
+            if (time_green_active){
+                cancel_repeating_timer(&timer_green);
+                gpio_put(LED_PIN_G , 0);
+                time_green_active = false;
+            }
+            else{
+                add_repeating_timer_ms(250 , timer_green_callback , NULL , &timer_green);
+                time_green_active = true;
+            }
+            
         }
     }
 }
