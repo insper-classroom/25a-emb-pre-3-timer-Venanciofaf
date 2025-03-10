7eaf7a43a00437b5745495969ee037aa81cc4043
Venanciofaf <venanciofaf@al.insper.edu.br>
2025-03-10 09:55:31 -0300

    Commit do Exercicio 1 Feito

--- exe1/main.c
+++ exe1/main.c
@@ -6,6 +6,14 @@ const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
 volatile int flag_f_r = 0;
+volatile bool time_active = false;
+
+bool timer_0_callback(repeating_timer_t *rt) {
+    bool current_state = gpio_get(LED_PIN_R);
+    gpio_put(LED_PIN_R , !current_state);
+    return true; 
+}
+
 
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) { // fall edge
@@ -26,10 +34,23 @@ int main() {
     gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
                                        &btn_callback);
 
+    repeating_timer_t timer_0;
+
     while (true) {
 
         if (flag_f_r) {
             flag_f_r = 0;
+        
+            if (time_active){
+                cancel_repeating_timer(&timer_0);
+                gpio_put(LED_PIN_R , 0);
+                time_active = false;
+            }
+            else{
+                add_repeating_timer_ms(500 , timer_0_callback , NULL , &timer_0);
+                time_active = true;
+            }
+
         }
     }
 }
